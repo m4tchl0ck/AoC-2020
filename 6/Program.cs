371b1a0b5628721c1e1fb30814df7451408bbf71
m4tchl0ck <m4tchl0ck@outlook.com>
2020-12-06 22:30:45 +0100

    Day 6 part one

---
+++ 6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var result = 0;
+using var file = new StreamReader(".\\input.txt");
+
+var group = new HashSet<char>();
+while (!file.EndOfStream)
+{
+    var input = (await file.ReadLineAsync()).ToCharArray();
+
+    if (input.Length == 0)
+    {
+        result += group.Count;
+        group.Clear();
+    }
+    else
+    {
+        foreach(var c in input)
+        {
+            group.Add(c);
+        }
+    }
+}
+
+result += group.Count;
+
+Console.WriteLine($"Result {result}");
