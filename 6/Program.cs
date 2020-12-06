826859a901a787381790186000b28512aea999ab
m4tchl0ck <m4tchl0ck@outlook.com>
2020-12-06 22:43:26 +0100

    Day 6 part two

--- 6/Program.cs
+++ 6/Program.cs
@@ -6,25 +6,32 @@ using System.Linq;
 var result = 0;
 using var file = new StreamReader(".\\input.txt");
 
-var group = new HashSet<char>();
+var group = new Dictionary<char, int>();
+var personsCount = 0;
 while (!file.EndOfStream)
 {
     var input = (await file.ReadLineAsync()).ToCharArray();
 
     if (input.Length == 0)
     {
-        result += group.Count;
+        result += group.Count(g => g.Value == personsCount);
         group.Clear();
+        personsCount = 0;
     }
     else
     {
+        personsCount++;
         foreach(var c in input)
         {
-            group.Add(c);
+            if (!group.ContainsKey(c))
+            {
+                group[c] = 0;
+            }
+            group[c]++;
         }
     }
 }
 
-result += group.Count;
+result += group.Count(g => g.Value == personsCount);
 
 Console.WriteLine($"Result {result}");
